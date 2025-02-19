2a49e2f8ba581af3b86ae69cdfa059a6f8be753e
futuredeveloper41 <mj10283@naver.com>
2025-02-19 09:38:18 +0900

    # action 함수로 post 요청 보내기

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import {Form, useNavigate} from 'react-router-dom';
 import styles from './EventForm.module.scss';
 
 const EventForm = () => {
@@ -6,48 +6,54 @@ const EventForm = () => {
     // 링크 이동시 새로고침 없이 이동하는 훅
     const navigate = useNavigate();
 
-    // 서버로 데이터 보내기
-    const handleSubmit = e => {
-        e.preventDefault();
-
-        // form에 입력한 값 가져오기
-        const formData = new FormData(e.target);
-
-        // 서버에 보낼 데이터 만들기
-        const payload = {
-            title: formData.get('title'),
-            desc: formData.get('description'),
-            imageUrl: formData.get('image'),
-            beginDate: formData.get('date')
-        };
-        // console.log(payload);
-
-        // 서버로 페칭
-        const fetchPost = async () => {
-            const response = await fetch(`http://localhost:9000/api/events`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(payload),
-            });
-
-            if (response.ok) {
-                // window.location.href = '/events';
-                navigate('/events');
-            }
-        };
-
-        fetchPost();
-
-
-    };
+    // # action 함수로 대체함 # //
+    // // 서버로 데이터 보내기
+    // const handleSubmit = e => {
+    //     e.preventDefault();
+    //
+    //     // form에 입력한 값 가져오기
+    //     const formData = new FormData(e.target);
+    //
+    //     // 서버에 보낼 데이터 만들기
+    //     const payload = {
+    //         title: formData.get('title'),
+    //         desc: formData.get('description'),
+    //         imageUrl: formData.get('image'),
+    //         beginDate: formData.get('date')
+    //     };
+    //     // console.log(payload);
+    //
+    //     // 서버로 페칭
+    //     const fetchPost = async () => {
+    //         const response = await fetch(`http://localhost:9000/api/events`, {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify(payload),
+    //         });
+    //
+    //         if (response.ok) {
+    //             // window.location.href = '/events';
+    //             navigate('/events');
+    //         }
+    //     };
+    //
+    //     fetchPost();
+    //
+    //
+    // };
 
     return (
-        <form
+        // Action : PUT, DELETE, DELETE 요청 시 사용
+        //  - 트리거 방법 :
+        //     1. form 태그를 대문자 Form 태그로 변경
+        //     2. method 옵션을 설정
+        <Form
+            method='POST'
             className={styles.form}
             noValidate
-            onSubmit={handleSubmit}
+            // onSubmit={handleSubmit}
         >
             <p>
                 <label htmlFor='title'>Title</label>
@@ -89,7 +95,7 @@ const EventForm = () => {
                 <button type='button'>Cancel</button>
                 <button>Save</button>
             </div>
-        </form>
+        </Form>
     );
 };
 
--- src/pages/NewEventPage.jsx
+++ src/pages/NewEventPage.jsx
@@ -5,3 +5,40 @@ const NewEventPage = () => {
 };
 
 export default NewEventPage;
+
+
+// Action : PUT, DELETE, DELETE 요청 시 사용
+//  - 트리거 방법 :
+//     1. form 태그를 대문자 Form 태그로 변경
+//     2. method 옵션을 설정
+//     3. parameter로 props.request 받고,
+//     4. "await" request.formData();로 form data의 데이터 가져오기
+//     5. redirect() 로 페이지 이동
+export const action = async ({request}) => {
+
+    console.log(request);
+
+    // action함수에서 form에 입력한 값 가져오기
+    const formData = await request.formData();
+    // 서버에 보낼 데이터 만들기
+    const payload = {
+        title: formData.get('title'),
+        desc: formData.get('description'),
+        imageUrl: formData.get('image'),
+        beginDate: formData.get('date'),
+    };
+    // console.log(payload);
+
+    // 서버로 페칭
+    const response = await fetch(`http://localhost:9000/api/events`, {
+        method: request.method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    });
+
+    // 다른페이지로 이동하는 방법
+    return redirect('/events'); // 목록페이지로 이동
+}
+

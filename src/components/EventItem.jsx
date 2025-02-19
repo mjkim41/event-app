40dd41ca2f3a9f81c02ecff73b9a344b12cef903
futuredeveloper41 <mj10283@naver.com>
2025-02-19 10:51:14 +0900

    # useSubmit+action 으로 Form 없이도 action trigger 하기

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -1,14 +1,36 @@
-
+import { Link, useNavigate, useSubmit } from 'react-router-dom';
 import styles from './EventItem.module.scss';
-import {Link} from "react-router-dom";
 
 const EventItem = ({ event }) => {
+
+    const navigate = useNavigate();
+
     const {
+        id,
         title,
         desc: description,
         'img-url': image,
         'start-date': date,
     } = event;
+
+    // action함수를 트리거하는 hook
+    const submit = useSubmit();
+
+    // 삭제 처리 이벤트
+    const handleRemove = e => {
+
+        // Form 없이 action함수를 트리거하는 방법 - 낙관적 업데이트
+        submit(null, { method: 'DELETE' });
+
+
+        // if (!confirm('정말 삭제하시겠습니까?')) return;
+
+        // (async () => {
+        //   const res = await fetch(`http://localhost:9000/api/events/${id}`, { method: 'DELETE' });
+        //   if (res.ok) navigate('/events');
+        // })();
+
+    };
 
     return (
         <article className={styles.event}>
@@ -21,7 +43,7 @@ const EventItem = ({ event }) => {
             <p>{description}</p>
             <menu className={styles.actions}>
                 <Link to='edit'>Edit</Link>
-                <button>Delete</button>
+                <button onClick={handleRemove}>Delete</button>
             </menu>
         </article>
     );

628bf5d13b1cc4560277516621b04f2cf2d617d0
futuredeveloper41 <mj10283@naver.com>
2025-02-19 12:29:33 +0900

    # 무한 스크롤링

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import EventList from "../components/EventList";
-import { useLoaderData } from "react-router-dom";
 
 const EventsPage = () => {
 
+    // 무한스크롤 옵저버가 감시할 태그 ref
+    const observerRef = useRef();
+
     // loader가 리턴한 데이터 받아오기
-    // const eventList = useLoaderData();
+    // const { eventList, hasNext } = useLoaderData();
 
     // 서버에서 가져온 화면에 렌더링할 이벤트 목록
     const [eventList, setEventList] = useState([]);
@@ -13,7 +15,7 @@ const EventsPage = () => {
     // 현재 페이지 번호
     const [currentPage, setCurrentPage] = useState(1);
 
-    // 더 가져올 데이터가 있는지 여부
+    // 더 이상 가져올 데이터가 있는지 여부
     const [isFinish, setIsFinish] = useState(false);
 
     // 서버에서 데이터를 불러오는 함수
@@ -24,22 +26,56 @@ const EventsPage = () => {
         const response = await fetch(`http://localhost:9000/api/events?sort=id&page=${currentPage}`);
         const { hasNext, eventList: events } = await response.json();
 
-        setEventList((prev) => [...prev, ...events]);
+        // 강제로 2초간 로딩 부여
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        setEventList(prev => [...prev, ...events]);
         setIsFinish(!hasNext);
-        // 페이지 번호 갱신
+        // 페이지번호 갱신
         setCurrentPage(prev => prev + 1);
     };
+
+
 
     // useEffect는 렌더링 이후에 실행됨
     useEffect(() => {
 
-      fetchEvents();
+        // 무한스크롤 옵저버 생성
+        const observer = new IntersectionObserver(([target]) => {
+            // entries는 옵저버가 감시하고 있는 대상들의 집합배열
+            if (!target.isIntersecting || isFinish) {
+                return;
+            }
+            fetchEvents();
+        }, {
+            // 관찰하고 있는 대상의 높이가 50% 보일 때 감지 실행
+            threshold: 0.5
+        });
+
+        // 옵저버의 감시 대상을 설정
+        if (observerRef.current) {
+            // 감시 설정
+            observer.observe(observerRef.current);
+        }
+
+        // 다 불러왔으면 감시 해제
+        return () => {
+            observer.disconnect();
+        };
 
     }, [currentPage]);
 
-    console.log('event page render!!');
+    // console.log('event page render!!');
 
-    return <EventList eventList={eventList} />;
+    return (
+        <>
+            <EventList eventList={eventList} />
+            {/* 무한스크롤을 위한 옵저버 감시 태그 */}
+            <div ref={observerRef} style={{ height: 300, background: 'yellow' }}>
+                {/* 로딩 바 or 로딩 프로그레스바 or 스켈레톤 폴백 */}
+            </div>
+        </>
+    );
 };
 
 export default EventsPage;
@@ -48,7 +84,7 @@ export default EventsPage;
 // export const loader = async () => fetch(`http://localhost:9000/api/events`);
 
 export const loader = async () => {
-    const res = await fetch(`http://localhost:9000/api/events?sort=id`);
+    const res = await fetch(`http://localhost:9000/api/events?sort=id&page=1`);
     // const jsonData = await res.json();
 
     // 예외처리

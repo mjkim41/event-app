839b8caa2e7b61451f07699b1d695d9afb80bd12
futuredeveloper41 <mj10283@naver.com>
2025-02-20 11:43:56 +0900

    # 로딩 프로그레스 바

--- src/components/auth/SignupForm.jsx
+++ src/components/auth/SignupForm.jsx
@@ -2,15 +2,25 @@ import styles from './SignUpForm.module.scss';
 import EmailInput from './EmailInput';
 import VerificationInput from "./VerificationInput.jsx";
 import {useState} from "react";
+import ProgressBar from "../common/ProgressBar.jsx";
 
 const SignUpForm = () => {
 
     // 현재 어떤 스텝에 위치하고 있는지
     const [step, setStep] = useState(1);
 
+    // 다음 스텝으로 넘어가는 여부
+    const [success, setSuccess] = useState(false);
+
     // 이메일 중복확인이 끝났을 떄 호출될 함수
     const emailSuccessHandler = () => {
-        setStep(2);
+
+        setSuccess(true);
+        setTimeout(() => {
+            setStep(2)
+            setSuccess(false);
+        }, 1000);
+
     }
 
     return (
@@ -19,6 +29,8 @@ const SignUpForm = () => {
 
                 {step === 1 && <EmailInput onSuccess={emailSuccessHandler}/>}
                 {step === 2 && <VerificationInput />}
+
+                {success && <ProgressBar />}
 
             </div>
         </div>

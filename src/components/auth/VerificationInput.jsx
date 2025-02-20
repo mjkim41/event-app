ec25036d461e52659cc09f8c97cd166ec3914f9f
futuredeveloper41 <mj10283@naver.com>
2025-02-20 12:30:27 +0900

    # autofocus

--- src/components/auth/VerificationInput.jsx
+++ src/components/auth/VerificationInput.jsx
@@ -1,34 +1,53 @@
+import { useEffect, useRef } from 'react';
 import styles from './SignUpForm.module.scss';
-import {useRef} from "react";
 
 const VerificationInput = () => {
 
     const inputsRef = useRef([]);
 
-    // input 태그들을 ref에 바인딩
+    // input태그들을 ref에 바인딩
     const bindInputRef = ($input, index) => {
-        console.log(index);
         inputsRef.current[index] = $input;
-    }
+    };
 
-    // 초기렌더링 시 첫번쨰 input에 포커싱
+    // 포커스를 다음칸으로 이동하는 함수
+    const focusNextInput = (index) => {
+        // 인덱스 검증 - 마지막 칸에서는 포커스 이동 금지
+        if (index < inputsRef.current.length) {
+            inputsRef.current[index].focus();
+        } else {
+            // 마지막 칸에서는 포커스 해제
+            inputsRef.current[index - 1].blur();
+        }
+    };
+
+    // 입력이벤트
+    const handleNumber = (e, index) => {
+
+
+        // 입력이 끝나면 다음 칸으로 포커스 이동
+        focusNextInput(index);
+    };
+
+    // 초기 렌더링시 첫번째 input에 포커싱
+    useEffect(() => {
+        inputsRef.current[0].focus();
+    }, []);
 
     return (
         <>
             <p>Step 2: 이메일로 전송된 인증번호 4자리를 입력해주세요.</p>
             <div className={styles.codeInputContainer}>
-                {
-                    Array.from(new Array(4)).map((_, index) => (
-                        <input
-                            ref={($input) => bindInputRef($input, index)}
-                            key={index}
-                            type='text'
-                            className={styles.codeInput}
-                            maxLength={1}
-                            onChange={(e) => console.log(inputsRef.current)}
-                        />
-                    ))
-                }
+                {Array.from(new Array(4)).map((_, index) => (
+                    <input
+                        ref={($input) => bindInputRef($input, index)}
+                        key={index}
+                        type='text'
+                        className={styles.codeInput}
+                        maxLength={1}
+                        onChange={e => handleNumber(e, index + 1)}
+                    />
+                ))}
             </div>
         </>
     );

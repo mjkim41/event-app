126c81e11189f4a05ff2113450d9f7fffe1564f0
futuredeveloper41 <mj10283@naver.com>
2025-02-20 12:18:37 +0900

    # 다중 ref 바인딩

--- src/components/auth/VerificationInput.jsx
+++ src/components/auth/VerificationInput.jsx
@@ -1,11 +1,34 @@
 import styles from './SignUpForm.module.scss';
+import {useRef} from "react";
 
 const VerificationInput = () => {
+
+    const inputsRef = useRef([]);
+
+    // input 태그들을 ref에 바인딩
+    const bindInputRef = ($input, index) => {
+        console.log(index);
+        inputsRef.current[index] = $input;
+    }
+
+    // 초기렌더링 시 첫번쨰 input에 포커싱
+
     return (
         <>
             <p>Step 2: 이메일로 전송된 인증번호 4자리를 입력해주세요.</p>
             <div className={styles.codeInputContainer}>
-                <input type='text' />
+                {
+                    Array.from(new Array(4)).map((_, index) => (
+                        <input
+                            ref={($input) => bindInputRef($input, index)}
+                            key={index}
+                            type='text'
+                            className={styles.codeInput}
+                            maxLength={1}
+                            onChange={(e) => console.log(inputsRef.current)}
+                        />
+                    ))
+                }
             </div>
         </>
     );

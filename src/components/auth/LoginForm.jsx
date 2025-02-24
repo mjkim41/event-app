21b17d2c1d578d98e0f1fa0285a135c2b34d2100
futuredeveloper41 <mj10283@naver.com>
2025-02-24 11:32:01 +0900

    # action 함수로 로그인 요청 보내기 및 실패 피드백

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,12 +1,15 @@
-import { Form, Link } from 'react-router-dom'; // Link 컴포넌트 추가
+import {Form, Link, useActionData} from 'react-router-dom'; // Link 컴포넌트 추가
 import styles from './LoginForm.module.scss';
 import {AUTH_API_URL} from "../../config/host-config.js";
 import {useState} from "react";
 
 const LoginForm = () => {
 
+    // action 함수가 리턴한 데이터 받아오기
+    const error = useActionData();
+
     // 에러 정보 상태 관리
-    const [error, setError] = useState('');
+    // const [error, setError] = useState('');
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -43,7 +46,7 @@ const LoginForm = () => {
     return (
         <>
             <Form
-                onSubmit={handleSubmit}
+                // onSubmit={handleSubmit}
                 method='post'
                 className={styles.form}
             >
@@ -84,3 +87,31 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
+
+// login 요청에 대한 action 함수
+ export const loginAction = async ({ request }) => {
+
+
+     // 입력 데이터 읽기
+     const formData = await request.formData();
+
+     const payload = {
+         email: formData.get('email'),
+         password: formData.get('password')
+     }
+
+     const response = await fetch(`${AUTH_API_URL}/login`, {
+         method: 'POST',
+         headers: {'Content-Type': 'application/json'},
+         body: JSON.stringify(payload),
+     });
+
+     const data = await response.json();
+     console.log(data);
+
+     if (response.status === 422) {
+         // 서버가 가져온 데이터를 컴포넌트에서 사용할 사용할 수 있도록 데이터 리턴
+         return data.message;
+     }
+ };
+

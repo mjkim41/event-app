4b1a7f361959fd1ec0679bdffc345cc540195d74
futuredeveloper41 <mj10283@naver.com>
2025-02-25 10:15:11 +0900

    # 인증 라우터 보호

--- src/config/auth-config.js
+++ src/config/auth-config.js
@@ -1,4 +1,4 @@
-import {redirect} from "react-router-dom";
+import {redirect, useSubmit} from "react-router-dom";
 
 const USER_DATA_KEY = 'userData';
 
@@ -14,8 +14,23 @@ export const userDataLoader = () => {
   return getUserData(); // loader가 리턴한 데이터는 loader가 걸린 페이지 및 하위 컴포넌트에서 사용가능
 };
 
+
 // 로그아웃 액션 함수
 export const logoutAction = () => {
   localStorage.removeItem(USER_DATA_KEY);
   return redirect('/');
+}
+
+// 로그인 여부를 알려주는 함수
+const isLoggedIn = () => {
+  return getUserData() !== null;
+};
+
+// 로그인 여부를 검사하여 로그인하지 않았따면 로그인페이지로 redirect 하는 로더
+export const authCheckLoader = () => {
+  if(!isLoggedIn()) {
+    alert('로그인 필요');
+    return redirect('/'); //  로그인 페이지로 이동
+  }
+  return null; // 현재 페이지에 머뭄
 }
